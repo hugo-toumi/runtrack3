968c01a149f8971560bb071b1709273b826fd8fd
hugo-toumi <hugo.toumi@laplateforme.io>
2022-03-15 15:39:39 +0100

    job 04

---
+++ jour02/job04/script.js
@@ -0,0 +1,12 @@
+document.addEventListener("DOMContentLoaded", (e) => {
+    
+    let textarea = document.getElementById('keylogger');
+
+    document.addEventListener("keypress", function(event){
+        
+        let key = event.key;
+
+        textarea.value += key;
+        
+    })
+})
